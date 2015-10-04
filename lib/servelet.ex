35d32d4b5153c28ef07c5eb6cdf316711cb1cc44
Andrew Nesbitt <andrewnez@gmail.com>
2015-10-04 18:44:33 +0100

    Parse mix.lock file

--- lib/servelet.ex
+++ lib/servelet.ex
@@ -4,6 +4,20 @@ defmodule Servelet do
   plug :match
   plug :dispatch
 
+  post "/lock" do
+    {:ok, data, _conn} = Plug.Conn.read_body(conn)
+
+    try do
+      json = data
+      |> Code.string_to_quoted
+      |> Encoder.to_json
+      send_resp(conn, 200, json)
+    rescue error
+      -> send_resp(conn, 422, ":(")
+    end
+  end
+
+
   post "/" do
     {:ok, data, _conn} = Plug.Conn.read_body(conn)
 
