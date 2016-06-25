3fa62e71bfcb1385847c295f3e84bedecd9da76d
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-25 15:25:44 +0100

    Upgrade to elixir 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mixup.Mixfile do
   def project do
     [app: :mixup,
      version: "0.0.1",
-     elixir: "~> 1.2.6",
+     elixir: "~> 1.3.0",
      deps: deps,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod]
