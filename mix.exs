0f39275f0bdbdf7169ee32ef21571ccfba285759
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-04 16:25:44 +0000

    Update to elixir 1.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mixup.Mixfile do
   def project do
     [app: :mixup,
      version: "0.0.1",
-     elixir: "~> 1.4.0",
+     elixir: "~> 1.4.1",
      deps: deps(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod]
