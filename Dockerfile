ac04243ed7bc0fb48a40756ed982643519c5ed67
Andrew Nesbitt <andrewnez@gmail.com>
2017-06-22 07:36:17 -0400

    Use elixir 1.4.4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM elixir:1.4.2
+FROM elixir:1.4.4
 RUN \
     apt-get update \
     && apt-get clean \
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mixup.Mixfile do
   def project do
     [app: :mixup,
      version: "0.0.1",
-     elixir: "~> 1.4.2",
+     elixir: "~> 1.4.4",
      deps: deps(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod]
