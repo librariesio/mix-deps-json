18b28995200b1d499a78d5f0047ed6a103dc7d32
Andrew Nesbitt <andrewnez@gmail.com>
2017-07-21 10:23:32 +0100

    Update plug

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Mixup.Mixfile do
   defp deps do
     [
       {:poison, "~> 3.1.0"},
-      {:plug, "~> 1.3.0"},
+      {:plug, "~> 1.4.0"},
       {:cowboy, "~> 1.1.0"},
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:inch_ex, "~> 0.5", only: :docs}
