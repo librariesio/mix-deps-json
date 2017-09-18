08a2c096794a2efaabbe1bd22e3be7acca442c60
Andrew Nesbitt <andrewnez@gmail.com>
2017-09-18 15:59:07 +0100

    Removed unneeded comments

--- mix.exs
+++ mix.exs
@@ -10,26 +10,10 @@ defmodule Mixup.Mixfile do
      start_permanent: Mix.env == :prod]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :cowboy, :plug]]
   end
 
-  # defp escript_config do
-  #   [main_module: Servelet]
-  # end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:poison, "~> 3.1.0"},
